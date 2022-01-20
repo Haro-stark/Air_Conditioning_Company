e50e352cde36781c00604a6e9d9aec7d9c2ca75a
fawad khan <fawad.khan@d-p-s.com>
2022-01-20 18:39:19 +0500

    fixed bugs

--- src/app/employees/employees.component.ts
+++ src/app/employees/employees.component.ts
@@ -1,17 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import * as Long from 'long';
 import { Employee } from '../models/Employee';
 
 @Component({
   selector: 'app-employees',
   templateUrl: './employees.component.html',
-  styleUrls: ['./employees.component.css']
+  styleUrls: ['./employees.component.css'],
 })
 export class EmployeesComponent implements OnInit {
+  employees: Employee[] = [
+    {
+      employeeId: Long.fromNumber(1),
+      type: 'emp',
+      username: 'fawad',
+      password: '12312',
+      priceTime: 69,
+      workLogList: [],
+    },
+  ];
+  constructor() {}
 
-  employees: Employee[] = [];
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
