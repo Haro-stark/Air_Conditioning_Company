f19f38bb6237542a0c8462ef4f186c70bd5a7801
fawad khan <fawad.khan@d-p-s.com>
2022-01-25 23:12:45 +0500

    change production setting

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebaseConfig: {
     apiKey: 'AIzaSyAvgnuL_vNM-tuYOWJe09wmntZ1YNuNeZ0',
     authDomain: 'airconditioning-authentication.firebaseapp.com',
