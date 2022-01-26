de20be61f1a65470401d80b96f9916164c36c42b
Haro-stark <haroonrb64@hotmail.com>
2022-01-26 15:45:34 +0500

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,7 @@ import { HomeComponent } from './home/home.component';
 import { OfficerComponent } from './officer/officer.component';
 import { OrdersComponent } from './orders/orders.component';
 import { ProductsComponent } from './products/products.component';
+import { AuthenticationService } from './service/authentication.service';
 import { SuppliersComponent } from './suppliers/suppliers.component';
 import { WorklogComponent } from './worklog/worklog.component';
 
@@ -65,5 +66,8 @@ const routes: Routes = [
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
+  providers: [
+    AuthenticationService
+  ]
 })
 export class AppRoutingModule { }
